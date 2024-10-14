ba4ec66c3be11a3784057a2ac3b17e225c510519
Matt <69976995+Matt54633@users.noreply.github.com>
2024-10-14 15:54:11 +0100

    Add Auth Healthcheck

---
+++ auth/src/main/java/com/project/auth/controllers/AuthController.java
@@ -0,0 +1,17 @@
+package com.project.auth.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AuthController {
+    
+    // Un-authenticated health check
+
+    @GetMapping
+    public String heartbeat() {
+        return "Auth service is running!";
+    }
+}
